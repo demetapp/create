3584a678daf434215e72d87f258d5a785b033f2d
demetapp <34214828+demetapp@users.noreply.github.com>
2017-12-05 10:53:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -531,25 +531,23 @@ function sendTextMessage(recipientId, messageText) {
       attachment: {
         type: "template",
         payload: {
-          template_type: "button",
-          text: "Mal ddoruk",
+          template_type: messageText,
+          text: "Merhaba Kilim Mobilya Facebook sayfasına Hoşgeldiniz.Size nasıl yardımcı olabilirim?",
           buttons:[{
-            type: "web_url",
-            url: "https://www.oculus.com/en-us/rift/",
-            title: "Open Web URL"
-          }, {
-            type: "postback",
-            title: "Trigger Postback",
+            type: "Ürünler Hakkında Bilgi alacağım",
+            title: "Bilgi almak istediğiniz ürün koleksiyonunun adını yazabilirmisiniz?",
             payload: "DEVELOPER_DEFINED_PAYLOAD"
           }, {
-            type: "phone_number",
-            title: "Call Phone Number",
-            payload: "+16505551234"
+            type: "Görüş bildireceğim",
+            title: "Kilim mobilya ile alakalı görüşleriniz bizim için çok değerli.Sorularınız , önerileriniz ve merak ettiklerinizi bize bildirin müşteri temsilcilerimiz en kısa sürede size ulaşsın.",
+            payload: "DEVELOPER_DEFINED_PAYLOAD"
           }]
         }
       }
-          metadata: "DEVELOPER_DEFINED_METADATA"
+            metadata: "DEVELOPER_DEFINED_METADATA"
     }
+        
+   
 
       
  
