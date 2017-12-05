0a4fd17e095b8e87d7bd0a481ea999283ff2fc1f
demetapp <34214828+demetapp@users.noreply.github.com>
2017-12-05 10:27:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -529,7 +529,11 @@ function sendTextMessage(recipientId, messageText) {
     },
     message: {
       text: "Merhaba Kilim Mobilya Facebook sayfasına Hoşgeldiniz.Size nasıl yardımcı olabilirim?",
-      buttons:[{
+         attachment: {
+        type: "template",
+        payload: {
+          template_type: messageText,
+          buttons:[{
             type: "Ürünler Hakkında Bilgi alacağım",
             title: "Bilgi almak istediğiniz ürün koleksiyonunun adını yazabilirmisiniz?",
             payload: "DEVELOPER_DEFINED_PAYLOAD"
@@ -538,6 +542,8 @@ function sendTextMessage(recipientId, messageText) {
             title: "Kilim mobilya ile alakalı görüşleriniz bizim için çok değerli.Sorularınız , önerileriniz ve merak ettiklerinizi bize bildirin müşteri temsilcilerimiz en kısa sürede size ulaşsın.",
             payload: "DEVELOPER_DEFINED_PAYLOAD"
           }]
+        }
+      }
       metadata: "DEVELOPER_DEFINED_METADATA"
    
     }
