7f4c39e9b1a9dff8b0187c6939862d664c3560ea
demetapp <34214828+demetapp@users.noreply.github.com>
2017-12-05 11:01:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -528,18 +528,7 @@ function sendTextMessage(recipientId, messageText) {
       id: recipientId
     },
     message:{
-    attachment:{
-      type:"template",
-      payload:{
-        template_type:"button",
-        text:"What do you want to do next?",
-        buttons:[
-          {
-            "type":"web_url",
-            "url":"https://www.messenger.com",
-            "title":"Visit Messenger"
-          } ]
-      }
+    text:"merhb"
     } metadata: "DEVELOPER_DEFINED_METADATA"
   }
  
